201bd71f8e93bedd15195d725204af7907d075f3
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-01 22:54:47 -0500

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,25 +1,14 @@
 require 'faker'
-
 class PokemonsController < ApplicationController
-    def create
-        unless pokemon_params[:trainer_id].nil?
-            default = {}
-            if Trainer.find(pokemon_params[:trainer_id]).pokemons.count < 6
-                if pokemon_params[:nickname].nil?
-                    default[:nickname] = Faker::Name.first_name
-                end
 
-                if pokemon_params[:species].nil?
-                    default[:species] = Faker::Pokemon.name
-                end
-                @pokemon = Pokemon.create(pokemon_params.merge(default))
-                render json: @pokemon, status: 201
-            else
-                render json: { error: "Party is Full!"}, status: 403
-            end
-        else
-            render json: { error: "Trainer not found"}, status: 404
-        end
+    def create
+
+        @pokemon = Pokemon.create(
+            nickname: Faker::Name.name,
+            species: Faker::Games::Pokemon.name,
+            trainer_id: params[:trainer_id]
+        )
+        render json: @pokemon
     end
 
     def destroy
@@ -28,7 +17,7 @@ class PokemonsController < ApplicationController
             @pokemon.destroy
             render json: @pokemon
         else
-            render json: { error: "Pokemon not Found!" }, status: 404
+            render json: { error: "Pokemon not Found!" }
         end
     end
 
